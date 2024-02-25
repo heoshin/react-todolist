ab3b7dc2872a741da49da18137f5f4802d9ebec7
허신 <ghtls050@gmail.com>
2024-02-25 23:40:59 +0900

    수정, 취소 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,23 @@ import './App.css'
 function App() {
   const [currentMessage, setCurrentMessage] = useState("")
   const [todoList, setTodoList] = useState([])
+  const [edittingTodoList, setEdittingTodoList] = useState([])
+
   const addTodo = message => {
     setCurrentMessage('')
     let copyTodoList = [...todoList]
     const getId = () => {
-      if (copyTodoList.length == 0)
-      {
+      if (copyTodoList.length == 0) {
         return 0
       }
-      else 
-      {
+      else {
         return copyTodoList[copyTodoList.length - 1].id + 1
       }
     }
 
     let todoItem = {
-      id : getId(),
-      message : message,
+      id: getId(),
+      message: message,
       date: new Date().getTime()
     }
     copyTodoList.push(todoItem)
@@ -31,6 +31,32 @@ function App() {
       return value.id != id
     })
     setTodoList(deletedTodoList)
+  }
+  const addEdittingTodo = (id) => {
+    let copyEdittingTodoList = [...edittingTodoList]
+    copyEdittingTodoList[id] = {}
+    setEdittingTodoList(copyEdittingTodoList)
+  }
+  const setMessageEdittingTodo = (id, message) => {
+    let copyEdittingTodoList = [...edittingTodoList]
+    copyEdittingTodoList[id].message = message
+    setEdittingTodoList(copyEdittingTodoList)
+  }
+  const deleteEdittingTodo = (id) => {
+    let copyEdittingTodoList = [...edittingTodoList]
+    copyEdittingTodoList.splice(id)
+    setEdittingTodoList(copyEdittingTodoList)
+  }
+  const applyEdittingTodo = (id) => {
+    let copyTodoList = [...todoList]
+    deleteEdittingTodo(id)
+    copyTodoList.map((item, index) => {
+      if (item.id == id)
+      {
+        item.message = edittingTodoList[id].message
+      }
+    })
+    setTodoList(copyTodoList)
   }
 
   return (
@@ -58,8 +84,28 @@ function App() {
             <span>{new Date(item.date).toString()}</span>
             <span>{item.message}</span>
             <button onClick={() => {
+              addEdittingTodo(item.id)
+            }}>수정</button>
+            <button onClick={() => {
               deleteTodoById(item.id)
             }}>삭제</button>
+            {
+              edittingTodoList[item.id] != undefined
+                ? <div>
+                  <input onChange={
+                    e => {
+                      setMessageEdittingTodo(item.id, e.target.value)
+                    }
+                  }></input>
+                  <button onClick={() => {
+                    applyEdittingTodo(item.id)
+                  }}>적용</button>
+                  <button onClick={() => {
+                    deleteEdittingTodo(item.id)
+                  }}>취소</button>
+                </div>
+                : null
+            }
           </div>
         ))
       }
